7c95db9ff6098fe9861738b24429f5f9c9f561a5
Hernan <hernangonzalez1373@gmail.com>
2024-10-17 12:16:33 -0300

    Se cambio la base de datos

--- tp_web.sql
+++ tp_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2024 a las 21:02:20
+-- Tiempo de generación: 17-10-2024 a las 17:13:51
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -28,23 +28,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `usuarios` (
-  `id_usuario` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
   `documento` int(8) NOT NULL,
   `email` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `id_vuelo` int(11) DEFAULT NULL
+  `id_vuelo` int(11) DEFAULT NULL,
+  `clave` text NOT NULL,
+  `administrador` tinyint(1) DEFAULT NULL,
+  `usuario_Id` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nombre`, `documento`, `email`, `id_vuelo`) VALUES
-(1, 'Raul', 42987925, 'EmailRaul@hotmail.com', 1),
-(2, 'Franco', 42982312, 'EmailFranco@hotmail.com', 1),
-(10, 'Jose', 12459863, 'EmailJose@hotmail.com', NULL),
-(12, 'Javier', 35698756, 'EmailJavier@hotmial.com', 2),
-(13, 'Pedro', 25478963, 'EmailPedro@hotmial.com', 2);
+INSERT INTO `usuarios` (`id`, `nombre`, `documento`, `email`, `id_vuelo`, `clave`, `administrador`, `usuario_Id`) VALUES
+(1, 'Raul', 42987925, 'EmailRaul@hotmail.com', 1, '', 0, 'Raul123'),
+(2, 'Franco', 42982312, 'EmailFranco@hotmail.com', 1, '', 0, 'Franco123'),
+(10, 'Jose', 12459863, 'EmailJose@hotmail.com', NULL, '', 0, 'Jose123'),
+(12, 'Javier', 35698756, 'EmailJavier@hotmial.com', 2, '', 0, 'Javier123'),
+(13, 'Pedro', 25478963, 'EmailPedro@hotmial.com', 2, '', 0, 'Pedro123');
 
 -- --------------------------------------------------------
 
@@ -57,19 +60,21 @@ CREATE TABLE `vuelos` (
   `salida` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
   `destino` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
   `avion` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `hs_salida` time(6) NOT NULL,
-  `hs_llegada` time(6) NOT NULL,
+  `hs_salida` time(3) NOT NULL,
+  `hs_llegada` time(3) NOT NULL,
   `fecha` date NOT NULL,
-  `precio` int(11) NOT NULL
+  `precio` int(11) NOT NULL,
+  `capacidad` int(3) NOT NULL,
+  `url_Imagen` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `vuelos`
 --
 
-INSERT INTO `vuelos` (`id`, `salida`, `destino`, `avion`, `hs_salida`, `hs_llegada`, `fecha`, `precio`) VALUES
-(1, 'Mar del Plata', 'Balcarce', 'Boing707', '15:00:00.000000', '15:40:00.000000', '2024-09-24', 15000),
-(2, 'Aereo parque', 'Usuhaia', 'Airbus320', '21:00:00.000000', '00:00:00.000000', '2024-09-30', 200000);
+INSERT INTO `vuelos` (`id`, `salida`, `destino`, `avion`, `hs_salida`, `hs_llegada`, `fecha`, `precio`, `capacidad`, `url_Imagen`) VALUES
+(1, 'Mar del Plata', 'Balcarce', 'Boing707', '15:00:00.000', '15:40:00.000', '2024-09-24', 15000, 45, NULL),
+(2, 'Aereo parque', 'Usuhaia', 'Airbus320', '21:00:00.000', '00:00:00.000', '2024-09-30', 200000, 45, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -79,7 +84,7 @@ INSERT INTO `vuelos` (`id`, `salida`, `destino`, `avion`, `hs_salida`, `hs_llega
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuario`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `DNI` (`documento`);
 
 --
@@ -96,7 +101,7 @@ ALTER TABLE `vuelos`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `vuelos`
